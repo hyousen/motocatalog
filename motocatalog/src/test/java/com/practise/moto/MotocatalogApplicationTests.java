4cbe40e8a3ffddcda89a43d8d4fdd74026794608
hyousen <hsakuchi1209@gmail.com>
2024-11-26 08:24:27 +0900

    first commit

---
+++ motocatalog/src/test/java/com/practise/moto/MotocatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.moto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
